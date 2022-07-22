599401c78a912cef197767548c9c001668d73f5a
dragonpuffle <109742476+dragonpuffle@users.noreply.github.com>
2022-07-22 12:55:34 +0300

    Add files via upload
    Stupeni это 1 номер
    Kurier это 2 номер
    Hakeri это 3 номер

---
+++ 23.1.hakeri.pas
@@ -0,0 +1,18 @@
+﻿var c:int64;
+procedure f(n:int64;q,w,e:boolean);
+begin
+if n=7 then q:=true;
+if n=34 then w:=true;
+if n=69 then e:=true;
+if n<77 then begin
+f(n+1,q,w,e);
+f(n*2,q,w,e);
+end
+else if n=77 then 
+if q then if w then if not e then c+=1;
+end;
+begin
+c:=0;
+f(1,false,false,false);
+println(c);
+end.
---
+++ 23.2.stupeni.pas
@@ -0,0 +1,19 @@
+﻿var c:int64;
+procedure f(n:int64;q,w,e:boolean);
+begin
+  if n=35 then q:=true;
+  if n=57 then w:=true;
+  if n=60 then e:=true;
+  if n<63 then begin
+    f(n+2,q,w,e);
+    f(n+3,q,w,e);
+  end
+  else if n>=63 then 
+    if not q then if not w then if not e then
+      c+=1;
+end;
+begin
+  c:=0;
+  f(0,false,false,false);
+  println(c);
+end.
